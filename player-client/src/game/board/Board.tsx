239187d0be0eb8bf84f77115beb72fde8d45cc57
Lucas Rodiadis <lucas2@live.com.au>
2023-07-22 09:56:31 +1000

    Title positioning somewhat working

--- player-client/src/game/board/Board.tsx
+++ player-client/src/game/board/Board.tsx
@@ -5,8 +5,8 @@ import { default as Location } from "./Location";
 import { LocationProps } from "./BoardTypes";
 
 const BOARD_LOCATIONS: LocationProps[] = [
-    { name: "Uttoxeter", colour: "#0000FF", pos: [0.546, 0.244], tiles: [{ industry1: "ManuGoods", industry2: "Beer" }, { industry1: "Cotton", industry2: "Beer" }] },
-    { name: "Derby", colour: "#3a5f3a", pos: [0.738, 0.055], tiles: [{ industry1: "Cotton", industry2: "ManuGoods" }, { industry1: "Iron" }, { industry1: "Cotton", industry2: "Beer" },] }
+    { name: "Uttoxeter", colour: "#0000FF", pos: [0.5655, 0.2745], tiles: [{ industry1: "ManuGoods", industry2: "Beer" }, { industry1: "Cotton", industry2: "Beer" }] },
+    { name: "Derby", colour: "#3a5f3a", pos: [0.7575, 0.2985], tiles: [{ industry1: "Cotton", industry2: "ManuGoods" }, { industry1: "Iron" }, { industry1: "Cotton", industry2: "Beer" },] }
 ];
 
 export default function Board() {
--- player-client/src/game/board/Location.tsx
+++ player-client/src/game/board/Location.tsx
@@ -8,13 +8,18 @@ import { TILE_WIDTH } from "../../utils/Constants";
 export default function Location(props: LocationProps) {
 	return (
 		<div className='game-board-location' style={{
-			left: `calc(${props.pos[0] * 100}% - ${TILE_WIDTH} / 2)`,
-			top: `calc(${props.pos[1] * 100}% - ${TILE_WIDTH} / 2)`
+			left: `${props.pos[0] * 100}%`,
+			top: `${props.pos[1] * 100}%`,
+			// bottom: `calc(100% - calc(${props.pos[1] * 100}% - ${TILE_WIDTH} / 2))`,
 		}}>
-			<div className="location-tile-container">
-				<>{props.tiles.map((tile) => { <Tile {...tile} /> })}</>
-			</div>
-			<div className="location-title-container">{props.name}</div>
+			{/* <div className="location-tile-container">
+				<>
+					{props.tiles.map((tile) => { return <Tile {...tile} /> })}
+				</>
+			</div> */}
+			<div className="location-title-container" style={{
+				backgroundColor: props.colour
+			}}>{props.name}</div>
 		</div >
 
 	)
